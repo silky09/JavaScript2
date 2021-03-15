f823309139a48f1ef0b931b00be56b810b7e882b
silky <silkysantosh09@gmail.com>
2021-03-15 11:39:04 +0100

    Added step2-4 hw

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,10 +1,21 @@
 'use strict';
-
+// Constructors use the keyword this
 function Dog() {
   // add your code here
+  this.name = "Rupert";
+  this.color = "white";
+  this.numLegs = 2
 }
 
 const hound = new Dog();
+hound.name;
+console.log(hound.name); // Rupert
+hound.name = "Berry";
+console.log(hound.name); // Berry
+hound.color; // white
+hound.numLegs = 2
+ 
+
 
 // Do not change or remove anything below this line
 module.exports = hound;
