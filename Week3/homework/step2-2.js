0b241dc04ee713a0a7854e42667acc3597d13a45
silky <silkysantosh09@gmail.com>
2021-03-15 10:03:26 +0100

    Added step2-2 hw

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -4,17 +4,29 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
 
   // Replace this comment and the next line with your code
-  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers);
+  for (let number = startIndex; number <= stopIndex; number++) {
+    numbers.push(number);
+    if (number % 3 === 0) {
+      sayThree(number);
+    }
+    if (number % 5 === 0) {
+      sayFive(number);
+    }
+  }
+
+  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers); //  10 15 [λ: sayThree] [λ: sayFive] [ 10, 11, 12, 13, 14, 15 ]
+  
 }
+
 
 function sayThree(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log(number); // 12, 15
 }
 
 function sayFive(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log(number); // 10, 15
 }
 
 threeFive(10, 15, sayThree, sayFive);
