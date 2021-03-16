8e6aef352fd69e927be8c3dcbf589503fbffa429
silky <silkysantosh09@gmail.com>
2021-03-16 12:17:51 +0100

    Added step3 hw

--- Week3/homework/step3.js
+++ Week3/homework/step3.js
@@ -2,7 +2,10 @@
 
 function createBase(base) {
   // Replace this comment and the next line with your code
-  console.log(base);
+   return function (number) {
+     return number + base;
+   }
+
 }
 
 const addSix = createBase(6);
