ce639b79c7c9373084a3d4e3acc3d3c618aaa6b5
silky <silkysantosh09@gmail.com>
2021-03-16 12:01:24 +0100

    Added step2-7 hw

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -21,3 +21,13 @@ f2(y);
 console.log(y);
 
 // Add your explanation as a comment here
+ 
+/**
+ * In the 1st function, variable x is assigned to 9 and its increment
+ * 9 to 10, but as x is a constant variable when 
+ * console.log calls for x, the function prints out the const variable value which is 9.
+ * 
+ * In the 2nd function, variable y is an object and function changes the value of 9 using key x . 
+ * It increment it to 10 and assign it to x. when the console.log calls for y, 
+variable y finds the x and the value 10 which is assigned to it.
+ */
