02b6906f9bb567827d15a88ca888a408164593aa
silky <silkysantosh09@gmail.com>
2021-03-16 11:48:23 +0100

    Added step2-6 hw

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -3,21 +3,41 @@
 const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
+let my2ndArr = [];
+
 function flattenArray2d(arr) {
   // Replace this comment and the next line with your code
-  console.log(arr);
+  
+  for (let i = 0; i < arr2d.length; i++) {
+    for (let j = 0; j < arr2d[i].length; j++) {
+      my2ndArr.push(arr2d[i][j]);
+    }
+    
+  }
+   return my2ndArr;
+
 }
+  
+let my3dArr = [];
 
 function flattenArray3d(arr) {
   // Replace this comment and the next line with your code
-  console.log(arr);
+   for (let i = 0; i < arr3d.length; i++) {
+    for (let j = 0; j < arr3d[i].length; j++) {
+      for (let k = 0; k < arr3d[j].length; k++) {
+        my3dArr.push(arr3d[i][j][k]);
+      }
+    }
+  }
+  return my3dArr;
+
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
 console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
 
 // Do not change or remove anything below this line
-module.exports = {
-  flattenArray2d,
-  flattenArray3d,
-};
+  module.exports = {
+    flattenArray2d,
+    flattenArray3d,
+  };
